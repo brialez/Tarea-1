dc29deb314727402ad8f7ea16188bd43ae276e77
Brian González <146209368+brialez@users.noreply.github.com>
2024-03-08 18:48:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//El programa deber ́a leer, de la entrada est ́andar, los mensajes recibidos desde el servidor por la
+//El programa deberá leer, de la entrada estándar, los mensajes recibidos desde el servidor por la
 //aplicación de mensajer ıa instantanea, cada uno en una línea. Estarán compuestos de entre 1 y 1.000
-//caracteres, formados  ́unicamente por letras del alfabeto inglées, espacios, 
+//caracteres, formados unicamente por letras del alfabeto inglées, espacios, 
 //signos de exclamación (solo de cierre, por supuesto), y otros signos ortográficos.
 
-//Por cada l ́ınea de la entrada, el programa deberá escribir ESGRITO si el texto tiene más signos
+//Por cada línea de la entrada, el programa deberá escribir ESGRITO si el texto tiene más signos
 //de exclamación que letras (del abecedario inglés), y escrito en caso contrario.
 
 function compare(input){
